689eecd97ccb779fe9af1e288f347f01099f0f84
jrgarciadev <jrgarciadev@gmail.com>
2021-01-23 19:21:07 -0300

    transitions removed from home

--- src/views/HomeForm/index.js
+++ src/views/HomeForm/index.js
@@ -1,9 +1,6 @@
-/* eslint-disable react/no-array-index-key */
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { withTheme } from 'styled-components';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { HERO_TITLE_DELAY } from '@lib/constants';
 import { ArrowRight } from 'react-iconly';
 import PropTypes from 'prop-types';
 import { Corner } from '@components';
@@ -30,42 +27,22 @@ function HomeForm({ theme }) {
     router.push('/portfolio/[username]', `/portfolio/${username}`);
   };
 
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => setIsMounted(true), HERO_TITLE_DELAY);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  const logo = <LargeLogo alt="DevCover Logo" src="/devcover-logo.svg" />;
-  const heroTitle = <HeroTitle>Just type your username and watch the magic</HeroTitle>;
-  const form = (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledInput
-        placeholder="Github Username"
-        name="username"
-        type="text"
-        onChange={handleChange}
-      />
-      <StyledButton type="submit">
-        <ArrowRight set="light" primaryColor={theme.bg.default} />
-      </StyledButton>
-    </StyledForm>
-  );
-
-  const items = [logo, heroTitle, form];
-
   return (
     <StyledContainer>
       <Corner />
-      <TransitionGroup component={null}>
-        {isMounted &&
-          items.map((item, i) => (
-            <CSSTransition key={i} classNames="fadeup" timeout={1000}>
-              <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
-            </CSSTransition>
-          ))}
-      </TransitionGroup>
+      <LargeLogo alt="DevCover Logo" src="/devcover-logo.svg" />
+      <HeroTitle>Just type your username and watch the magic</HeroTitle>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledInput
+          placeholder="Github Username"
+          name="username"
+          type="text"
+          onChange={handleChange}
+        />
+        <StyledButton type="submit">
+          <ArrowRight set="light" primaryColor={theme.bg.default} />
+        </StyledButton>
+      </StyledForm>
     </StyledContainer>
   );
 }
