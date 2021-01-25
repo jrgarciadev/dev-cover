e5484e76b85d7102edcf48d4c39b00aa483358c6
jrgarciadev <jrgarciadev@gmail.com>
2021-01-25 02:34:01 -0300

    chore: improvements in github data fetching

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -2,16 +2,6 @@ import styled from 'styled-components';
 
 export const StyledAboutSection = styled.section`
   max-width: 100%;
-
-  .inner {
-    display: grid;
-    grid-template-columns: 3fr 2fr;
-    grid-gap: 50px;
-
-    @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-      display: block;
-    }
-  }
 `;
 
 export const StyledMarkdown = styled.div`
@@ -38,9 +28,13 @@ export const StyledMarkdown = styled.div`
     color: ${(props) => props.theme.text.accent};
     font-size: ${(props) => props.theme.fontSize.lg};
     margin: 0.5rem 0;
+    a {
+      font-size: ${(props) => props.theme.fontSize.lg};
+    }
   }
 
   a {
+    font-size: ${(props) => props.theme.fontSize.md};
     transition: ${(props) => props.theme.transitions.default};
     &:hover {
       color: ${(props) => props.theme.brand.primary};
