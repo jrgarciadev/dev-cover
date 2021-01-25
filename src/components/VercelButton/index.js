183269fa45383f6f94b2308d84e9604ccbd6ad83
jrgarciadev <jrgarciadev@gmail.com>
2021-01-25 01:37:21 -0300

    chore: vercel button descriptions for env

--- src/components/VercelButton/index.js
+++ src/components/VercelButton/index.js
@@ -5,12 +5,14 @@ const VercelButton = () => {
   const repository = 'https%3A%2F%2Fgithub.com%2Fjrgarciadev%2Fdev-cover';
   const variables = `&env=NEXT_PUBLIC_USERNAME`;
   const projectName = '&project-name=my-awesome-portfolio';
+  const repositoryName = '&repository-name=my-awesome-portfolio';
+  const envDescription = '&envDescription=Enter%20your%20Github%20username%20.';
   return (
     <ButtonContainer>
       <a
         rel="noreferrer"
         target="_blank"
-        href={`${deployUrl}${repository}${variables}${projectName}`}
+        href={`${deployUrl}${repository}${variables}${projectName}${repositoryName}${envDescription}`}
       >
         <img src="https://vercel.com/button" alt="Deploy with Vercel" />
       </a>
