63e7d9ee933701e68eb0892b63e55cc614eb7c4a
jrgarciadev <jrgarciadev@gmail.com>
2021-02-04 19:50:40 -0300

    chore: remove no necessary files

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { default as Menu } from './Menu';
 export { default as Social } from './Social';
 export { default as Side } from './Side';
-export { default as Email } from './Email';
 export { default as Hero } from './Hero';
 export { default as About } from './About';
 export { default as Blog } from './Blog';
